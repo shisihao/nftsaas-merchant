1d45f941e6e1e2916d02e13b6488dfcf5634c68b
ThirteenC <192537723@qq.com>
2022-11-02 11:31:43 +0800

    feat:商品订单新增藏品数量展示

--- src/views/shop-orders/index.vue
+++ src/views/shop-orders/index.vue
@@ -185,6 +185,9 @@
             <div>
               {{ integral }}：{{ row.integral_price || 0 }}
             </div>
+            <div>
+              藏品数量：{{ row.goods_num || 0 }}
+            </div>
             <div>购买量：{{ row.num }}</div>
             <div>
               <span v-if="row.pay_type === 'free'">
@@ -311,7 +314,7 @@ export default {
       },
       dateRangeValue: [],
       pickerOptions,
-      payOptions: payOptions.concat(payTypeOptions),
+      payOptions: payOptions.concat(payTypeOptions).concat({ label: '藏品兑换', value: 'voucherpay' }),
       shopStatusOptions,
       typeOptions,
       loading: false,
